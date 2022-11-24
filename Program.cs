30a5aececd2275c9359367e6ceeb00edbfbf5dbc
oriolli <mar.orioli@hotmail.com>
2022-11-24 11:08:05 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+      var data = DateTime.Now;
+      var formatada = String.Format("{0:dd/MM/yyyy hh:mm:ss ff z}", data);
+      var alterada = String.Format("{0:D}", data);
+      var change = String.Format("{0:r}", data);
+      var changa = String.Format("{0:s}", data);
+      var chango = String.Format("{0:u}", data);
+
+      Console.WriteLine(formatada);
+      Console.WriteLine(alterada);
+      Console.WriteLine(change);
+      Console.WriteLine(changa);
+      Console.WriteLine(chango);
+    }
+  }
+}
