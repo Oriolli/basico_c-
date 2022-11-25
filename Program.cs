15d899354fa82f31be9599f914697a0304d8b159
oriolli <mar.orioli@hotmail.com>
2022-11-25 11:08:32 -0300

    alteração de datas

--- Program.cs
+++ Program.cs
@@ -14,11 +14,24 @@ namespace MyApp
       var changa = String.Format("{0:s}", data);
       var chango = String.Format("{0:u}", data);
 
+      if (data.Date > DateTime.Now.Date)
+      {
+        Console.WriteLine("É igual");
+      }
+      else
+      {
+        Console.WriteLine("Não é igual");
+      }
+
       Console.WriteLine(formatada);
       Console.WriteLine(alterada);
       Console.WriteLine(change);
       Console.WriteLine(changa);
       Console.WriteLine(chango);
+      Console.WriteLine(data);
+      Console.WriteLine(data.AddDays(-12));
+      Console.WriteLine(data.AddMonths(1));
+      Console.WriteLine(data.AddYears(1));
     }
   }
 }
