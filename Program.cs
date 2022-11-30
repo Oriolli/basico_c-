fcf816cdbd8be7a63e61d6de040c117b2b826761
oriolli <mar.orioli@hotmail.com>
2022-11-30 14:40:30 -0300

    aprendendo_math

--- Program.cs
+++ Program.cs
@@ -41,12 +41,17 @@ namespace MyApp
       var atual = CultureInfo.CurrentCulture;
 
 
-      decimal valor = 10.25m;
+      decimal valor = 10536.25m;
       Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+      Console.WriteLine(Math.Round(valor));
+      Console.WriteLine(Math.Ceiling(valor));
+      Console.WriteLine(Math.Floor(valor));
       //C - exibe como moeda
       //N - somente o número sem formatação de moeda
       //P - exibe a porcentagem
-
+      //Math.Round - arredonda o valor
+      //Math.Ceiling - arredonda pra cima
+      //Math.Floor - arredonda pra baixo
 
       if (data.Date > DateTime.Now.Date)
       {
