4c0c6d8b95fed42999f60e5798cc44fa294f204a
oriolli <mar.orioli@hotmail.com>
2022-12-06 11:07:33 -0300

    exceptions

--- Program.cs
+++ Program.cs
@@ -9,6 +9,31 @@ namespace MyApp
     {
       Console.Clear();
 
+      var arr = new int[3];
+      try
+      {
+        for (var index = 0; index < 10; index++)
+        {
+          Console.WriteLine(arr[index]);
+        }
+      }
+      catch (IndexOutOfRangeException ex)
+      {
+        Console.WriteLine(ex.InnerException);
+        Console.WriteLine(ex.Message);
+        Console.WriteLine("Não encontrei o índice na lista");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.InnerException);
+        Console.WriteLine(ex.Message);
+        Console.WriteLine("Ops, algo deu errado!");
+      }
+      finally
+      {
+        Console.WriteLine("Chegou ao fim!");
+      }
+
       var data = DateTime.Now;
       var dateTime = DateTime.UtcNow;
       var timezoneAustralia = TimeZoneInfo.FindSystemTimeZoneById("Pacific/Auckland");
@@ -96,6 +121,11 @@ namespace MyApp
 
       Console.WriteLine(DateTime.Now.ToString("D", en));
       //Console.WriteLine(string.Format("{0:D}", DateTime.Now));
+    }
+    static void Salvar(String texto)
+    {
+      if (string.IsNullOrEmpty(texto))
+        throw new ArgumentNullException("O texto não pode ser nulo ou vazio");
     }
   }
 }
